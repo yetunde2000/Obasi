d0364615a5bc3703b8d51755ac8157ee7a7b53d5
obasi yetunde <olaopin_5@dgist.ac.kr>
2025-12-31 14:32:04 +0900

    changed

--- src/data/research.ts
+++ src/data/research.ts
@@ -10,16 +10,8 @@ export interface ResearchData {
 
 export const researchData: ResearchData = {
   interests: [
-    {
-      title: "Affective Computing",
-      description: "Analyzing emotional and mental states using multimodal signals such as speech and physiological data",
-      keywords: ["Multimodal Data Analysis", "Emotion Recognition", "Mental Health"]
-    },
-   {
-     title: "Interactive System",
-     description: "Designing interactive systems and visualizations to support digital mental health monitoring and interpretation",
-     keywords: ["Interactive System", "Visualization", "Data Storytelling"]
-   },
+    
+   
     // {
     //   title: "Signal Processing",
     //   description: "Developing signal processing methods to improve the quality and usability of real-world sensor data",
