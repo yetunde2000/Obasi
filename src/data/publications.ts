b7d7aa666a223ba80f2255c1fbcea1fe9ed4bb5a
obasi yetunde <olaopin_5@dgist.ac.kr>
2026-01-05 19:54:42 +0900

    changed

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -3,7 +3,7 @@ export interface Publication {
   authors: string[];
   venue: string;
   year: string;
-  type: 'journal' | 'conference' | 'workshop' | 'thesis';
+  type: 'journal' | 'Conference' | 'workshop' | 'thesis';
   doi?: string;
   link?: string;
   slides?: string;
@@ -18,27 +18,14 @@ export interface PublicationsData {
 
 export const publicationsData: PublicationsData = {
   publications: [
-    // {
-    //   title: "A Multimodal Sensor Fusion Approach Using Mobile, Wearable, and IoT Sensors for Mental Health Detection",
-    //   authors: ["Youngji Koh", "Gyuna Kim", "Chanhee Lee", "Panyu Zhang",
-    //     "Yunhee Ku", "Inhwan Choi", "Jewoo Ryu", "Uichin Lee"],
-    //   venue: "IEEE Internet of Things Journal",
-    //   year: "2025",
-    //   type: "journal",
-    //   doi: "10.1109/THMS.2024.3390000",
-    //   slides: "/images/publications/slides.pdf",
-    //   poster: "/images/publications/poster.pdf",
-    //   video: "/images/publications/video.mp4",
-    //   github: "https://github.com/Kaist-ICLab/multimodal-mh-detection",
-    // },
-    // {
-    //   title: "EmoWorker: A Multimodal Dataset for Assessing Emotion, Stress, and Emotional Workload in Interpersonal Work Scenario",
-    //   authors: ["Duri Lee", "Eunji Park", "Gyuna Kim", "Yunjo Han", "Uichin Lee"],
-    //   venue: "Scientific Data",
-    //   year: "2025",
-    //   type: "journal",
-    //   // doi: "10.1038/s41597-025-02455-x",
-    //   github: "https://github.com/Kaist-ICLab/EmoWorker",
-    // }
+     {
+      title: "A Novel Framework for Enhanced Flat Object Manipulation through Reinforcement Learning and Mixed Reality Integration",
+      authors: ["John Prada", "Obasi Yetunde", "Cheol Song", ],
+      venue: "DBpia",
+      year: "2023-11-24",
+      type: "Conference",
+      link: "http://hdl.handle.net/20.500.11750/57706",
+    },
+    
   ]
 }; 
