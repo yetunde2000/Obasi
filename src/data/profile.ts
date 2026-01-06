eee4f7ab86b22d9a840bf4531b2c99b90043e5ee
obasi yetunde <olaopin_5@dgist.ac.kr>
2026-01-06 12:52:12 +0900

    change

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -74,7 +74,8 @@ export const profileData: Profile = {
   skills: [ "Python", "Solidworks", "Unity", "Isaac sim", "Latex", "LabVIEW"
   ],
   about: `I am a  post-master’s researcher  at [DGIST-IROM](https://sites.google.com/view/dgist-ibom), in the Department of Robotics and Mechatronics Engineering. 
-My work focuses on Virtual Reality and Mixed Reality, where I explore human–computer interaction through immersive and interactive systems. I have experience designing and implementing AI-driven systems, with a strong background in deep learning and machine learning applied to real-world research problems.
+My research focuses on virtual reality–based systems for microsurgical applications, where I develop deep learning–driven methods to enhance interaction accuracy, sensor reliability, and human–machine performance.
+
 my cv can be found [here](/Obasi Yetunde_cv.pdf).`,
 
 
