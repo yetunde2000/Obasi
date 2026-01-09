d20f0cf53de3b7c6abcd5fc358445dea36c1439a
obasi yetunde <olaopin_5@dgist.ac.kr>
2026-01-09 10:51:02 +0900

    change

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -3,7 +3,7 @@ export interface Publication {
   authors: string[];
   venue: string;
   year: string;
-  type: 'journal' | 'Conference' | 'workshop' | 'thesis';
+  type: 'journal' | 'Conference' | 'workshop' |  'Writing sample' |'thesis';
   doi?: string;
   link?: string;
   slides?: string;
@@ -14,6 +14,7 @@ export interface Publication {
 
 export interface PublicationsData {
   publications: Publication[];
+  
 }
 
 export const publicationsData: PublicationsData = {
@@ -25,6 +26,14 @@ export const publicationsData: PublicationsData = {
       year: "2023-11-24",
       type: "Conference",
       link: "http://hdl.handle.net/20.500.11750/57706",
+    },
+    {
+      title: "Understanding Why People Struggle to Integrate RobotsInto Daily Life",
+      authors: [ "Obasi Yetunde Oluwatoyosi"],
+      venue: "Unpublished Work",
+      year: "2025-11-24",
+      type: "Writing sample",
+      link: "https://drive.google.com/file/d/11_7Ho8WtldXfsTZYEK7kMae96bhojh3z/view?usp=sharing",
     },
     
   ]
